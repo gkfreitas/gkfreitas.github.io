181bb97ffd3c299e82cc6241ff8fed801cffd8d6
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-10-05 20:50:17 -0300

    Header para desktop e mobile criados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Header from '@/components/Header'
 
 export default function Home() {
   return (
-    <>
+    <div className='max-w-[1280px] mx-auto'>
       <Header/>
-    </>
+    </div>
   )
 }
