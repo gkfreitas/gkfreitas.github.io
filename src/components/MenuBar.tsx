207194985c8cdb2b2e61dc8d247c774e2c6bce64
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-10-16 10:48:10 -0300

    Email funcionando

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -11,7 +11,7 @@ export default function MenuBar(props: MenuBarProps) {
   }
   return (
 
-      <ul className={`overflow-hidden text-center border-b pb-[10px] dark:border-[#404040] border-[#DDD6FE] sm:hidden ${props.display ? 'block duration-300 h-[180px]' : 'h-0 duration-300'} `}>
+      <ul className={`overflow-hidden text-center border-b pb-[10px] dark:border-[#404040] border-[#DDD6FE] sm:hidden ${props.display ? 'block duration-300 h-[140px]' : 'h-0 duration-300'} `}>
         {links.map((link) => (
         <li onClick={() => handleScroll(link)} className="font-medium cursor-pointer text-[18px] mt-[15px] dark:hover:text-[#C4B5FD] duration-200 hover:text-[#7C3AED]" key={link}>{link}</li>
         ))}
