5f708ab8195f2d3b3739944403f832d0077f8789
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-10-16 14:12:04 -0300

    Sistema de filtro feito e primeiro projeto públicado

---
+++ src/utils/projectsList.ts
@@ -0,0 +1,19 @@
+import nextJsIcon from '../../public/favicons/techIcons/next-js.svg'
+import reactIcon from '../../public/favicons/techIcons/react.svg'
+import tailwindIcon from '../../public/favicons/techIcons/tailwind-css.svg'
+import typescriptIcon from '../../public/favicons/techIcons/typescript.png'
+
+import portifiolioPreview from '../../public/preview/PreviewPortifolio.png'
+
+const portifolio = {
+  imagePreview: portifiolioPreview,
+  githubLinkCode: 'https://github.com/gkfreitas/gkfreitas.github.io',
+  liveAppLink: 'https://gkfreitas.github.io/',
+  alt: 'Preview do site do meu portifólio',
+  name: 'Portifólio',
+  description: 'Portifólio que apresenta minhas habilidades como programador e projetos feitos por mim.',
+  techIcons: [reactIcon, nextJsIcon, typescriptIcon, tailwindIcon],
+  stack: 'Front End',
+}
+
+export const projetcs = [portifolio]
